d01648f2b1e13b41d8f20f1250117d54767adaee
miftahhadi817 <ajago6028@gmail.com>
2021-06-07 05:55:54 +0100

    Membuat Class Main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Miftah";
+        String namaTengah = "Hadi";
+        String namaBelakang = "Syah";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+        }
+    }
